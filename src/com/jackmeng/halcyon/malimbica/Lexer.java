76e6622099ad8aa4b34447711792d950ae14a037
Jack Meng <jackmeng0814@gmail.com>
2022-07-23 02:53:03 +0000

    lexing edition

--- src/com/jackmeng/halcyon/malimbica/Lexer.java
+++ src/com/jackmeng/halcyon/malimbica/Lexer.java
@@ -2,14 +2,24 @@ package com.jackmeng.halcyon.malimbica;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.StringTokenizer;
 
 public class Lexer {
   private Map<String, String[]> content;
+  public static final char LEXER_FORMAT = '1';
   public Lexer(String objects) {
-    content = new HashMap<>();
+    content = init(objects);
   }
 
-  private static Map<String, String[]> init(String obj) {
-      return null;
+  private void identify(String obj) throws LexingException {
+    if(obj.charAt(0) != LEXER_FORMAT) {
+      throw new LexingException("Incorrect format spotted for this lexer!!\nGot: " + obj.charAt(0) +"\nMine: " + LEXER_FORMAT);
+    }
+  }
+
+  private Map<String, String[]> init(String obj) throws LexingException {
+    identify(obj);
+    StringTokenizer st = new StringTokenizer(obj);
+      
   }
 }
---
+++ src/com/jackmeng/halcyon/malimbica/LexingException.java
@@ -0,0 +1,7 @@
+package com.jackmeng.halcyon.malimbica;
+
+public class LexingException extends Exception {
+    public LexingException(String message) {
+        super(message);
+    }
+}
